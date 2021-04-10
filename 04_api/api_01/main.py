430dee9aabb440b346cdb335083e667009dbe35d
Vitor <vfarias@bionexo.com>
2021-04-10 14:37:43 -0400

    update api 01

---
+++ 04_api/api_01/main.py
@@ -0,0 +1,30 @@
+import uvicorn
+from fastapi import FastAPI
+from controllers.product_controller import PRODUCT_CONTROLLER
+
+app = FastAPI(
+    version='0.1.0',
+    title='Training API',
+    description='API for training',
+    docs_url='/',
+)
+
+
+@app.get('/endpoint_test')
+def endpoint_test():
+    return 'test: hello'
+
+
+# region Routers/Controllers
+app.include_router(
+    PRODUCT_CONTROLLER,
+    prefix='/v1/product',
+    tags=['product']
+)
+
+
+# Init do package
+if __name__ == '__main__':
+    uvicorn.run('main:app', host="localhost", port=7000, reload=True)
+
+
