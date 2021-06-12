62dbc157b34fd3bbc7fec55c649266458a4e68aa
Vitor <vfarias@bionexo.com>
2021-06-12 17:59:37 -0400

    update dict

--- 01_inity/dict.py
+++ 01_inity/dict.py
@@ -1,16 +1,20 @@
 # Armazenamento de lista de chave e valor
 
-d = dict()
+d1 = dict()
 
-d['chave1'] = 'valor 1'
-d['chave2'] = 'valor 2'
+d1['Nome'] = 'Vitor'
+d1['Idade'] = 31
 
-print(d)
+print(d1)
+print(d1["Nome"])
+print(d1["Idade"])
 
 
 
+# Declaração direta:
 
 d2 = {"Nome": "Vitor", "Idade": 31}
+
 print(d2)
 print(d2["Nome"])
 print(d2["Idade"])
