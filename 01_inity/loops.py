7ed9faf63822e1c5fd14eaccfc2485a1717bfd7d
Vitor <vfarias@bionexo.com>
2021-06-12 17:56:49 -0400

    update loops

--- 01_inity/loops.py
+++ 01_inity/loops.py
@@ -1,16 +1,31 @@
-lista = list()
-lista.append('item_a')
-lista.append('item_b')
-lista.append('item_c')
-
-for item in lista:
-    print(item)
+class Product:
+    def __init__(self, cod, name):
+        self.cod = cod
+        self.name = name
 
 
+listaA = []
+listaB = []
 
-i = 1
-while i < 6:
-  print(i)
+# Gerar conteúdo das listas (duas formas):
+for i in [0, 1, 2, 3, 4]:
+    product = Product(i, 'prod'+str(i))
+    listaA.append(product)
+
+for i in range(0, 5):
+    product = Product(i, 'prod'+str(i))
+    listaB.append(product)
+
+
+# Percorrer listas imprimindo name of product (duas formas):
+for product in listaA:
+    print(product.name)
+
+i = 0
+while i < len(listaB):
+  print(listaA[i].name)
   i += 1
+
+
 
 
--- 01_inity/product.py
+++ 01_inity/product.py
@@ -10,8 +10,6 @@ for i in [0, 1, 2, 3, 4]:
     prod = Product(i, 'prod'+str(i))
     lista.append(prod)
 
-i = 0
-while i < len(lista):
-    print(lista[i].name)
-    i = i + 1
+for product in lista:
+    print(product.name)
 
