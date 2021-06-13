6e64be0b68d0cc2d5a3a04aa79ffe67148653701
Vitor <vfarias@bionexo.com>
2021-06-12 22:55:53 -0400

    add interpolacao string

---
+++ 01_fundamentals/interpolacao.py
@@ -0,0 +1,16 @@
+print("Tentativa {} de {}".format(3, 10))
+
+print("Tentativa {0} de {1}".format(3, 10))
+
+print("Tentativa {1} de {0}".format(3, 10))
+
+print("R$ {}".format(5.99))
+
+print("R$ {:f}".format(5.99))
+
+print("R$ {:.2f}".format(5))
+
+print("R$ {:7.2f}".format(5))
+
+print("R$ {:11.3f}".format(5))
+
