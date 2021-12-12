a56bee341907acb46e1de1ba3aaa7df82b54363b
Vitor <vfarias@bionexo.com>
2021-12-12 00:12:06 -0400

    update

---
+++ 01_fundamentos/00_tipos.py
@@ -0,0 +1,8 @@
+texto: str
+contador: int
+preco: float
+complexo: complex
+emaior: bool
+lista: list
+tupla: tuple
+dicionario: dict
--- 01_fundamentos/09_funcoes.py
+++ 01_fundamentos/09_funcoes.py
@@ -1,19 +1,29 @@
+# funcao simples
 def printValue(value):
     print(value)
 
+# funcao com argumento tipado
 def printValue(value: str):
     print(value)
 
-def printValue(value: int):
-    print(value)
-
+# funcao com argumento tipado e retorno não tipado
 def somar(num1: float, num2: float):    
     return num1 + num2
 
-def somar(num1: float, num2: float) -> int:    
+# funcao com argumento e retorno tipados
+def somar(num1: float, num2: float) -> float:    
     return num1 + num2
 
 
 printValue(5.99)
 
-print(somar(5, 5.5))
+result = somar(5, 5.5)
+print(result)
+
+
+# Callback
+def calcular(num1, num2, operacao):
+    result = operacao(num1, num2)
+    print(result)   
+
+calcular(5, 3, somar)
